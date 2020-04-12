a2ee25b5d4daf12a7774d118aff4148f5dcd3ca1
stepm <stepm@LAPTOP-MH38KM44>
2020-04-12 18:53:41 +0900

    컨테이너 객체관리

--- 170320_spring01/src/spring01/Test.java
+++ 170320_spring01/src/spring01/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
+		// 컨테이너 빌드
 		ApplicationContext ctxt = new GenericXmlApplicationContext("applicationContext.xml");
 		
 		System.out.println("???");
