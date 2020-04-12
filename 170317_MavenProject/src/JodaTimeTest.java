b90c77edaea4a0fd84c7089de97cca91b1482ce8
stepm <stepm@LAPTOP-MH38KM44>
2020-04-12 18:13:43 +0900

    Maven 프로젝트 생성

---
+++ 170317_MavenProject/src/JodaTimeTest.java
@@ -0,0 +1,10 @@
+import org.joda.time.DateTime;
+
+public class JodaTimeTest {
+	
+	public static void main(String[] args) {
+		DateTime date = new DateTime();
+		System.out.println(date);
+	}
+	
+}
