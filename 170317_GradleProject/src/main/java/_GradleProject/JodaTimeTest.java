2028ac9c9eb86be2b4b3d096bbde05618e181f97
stepm <stepm@LAPTOP-MH38KM44>
2020-04-12 18:09:12 +0900

    Gradle 프로젝트 생성

---
+++ 170317_GradleProject/src/main/java/_GradleProject/JodaTimeTest.java
@@ -0,0 +1,12 @@
+package _GradleProject;
+
+import org.joda.time.DateTime;
+
+public class JodaTimeTest {
+	
+	public static void main(String[] args) {
+		DateTime date = new DateTime();
+		System.out.println(date);
+	}
+	
+}
