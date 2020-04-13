53997689cc45cb60cede7600e76715517220c90b
stepm <stepm@LAPTOP-MH38KM44>
2020-04-14 06:54:53 +0900

    컨테이너 객체관리

--- 170320_spring01/src/spring01/Test.java
+++ 170320_spring01/src/spring01/Test.java
@@ -8,11 +8,13 @@ public class Test {
 	public static void main(String[] args) {
 		
 		// 컨테이너 빌드
-		ApplicationContext ctxt = new GenericXmlApplicationContext("applicationContext.xml");
+		ApplicationContext ctxt = new GenericXmlApplicationContext("spring01/applicationContext.xml");
 		
 		System.out.println("???");
 		
 		// Greeting gr = (Greeting) ctxt.getBean("gr");
+		// gr.greeting();
+		
 		Greeting gr1 = ctxt.getBean("gr", Greeting.class);
 		gr1.greeting();
 		Greeting gr2 = ctxt.getBean("gr", Greeting.class);
@@ -20,12 +22,6 @@ public class Test {
 		
 		System.out.println(gr1);
 		System.out.println(gr2);
-		
-		if(gr1 == gr2) {
-			System.out.println("같은 객체");
-		} else {
-			System.out.println("다른 객체");
-		}
 		
 		// 빈 설정에 scope="prototype" 설정되어 있으면 소멸 동작 실행되지 않는다.
 		((GenericXmlApplicationContext) ctxt).close();
